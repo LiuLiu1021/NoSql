a7ac608e43704e6c6ebfa6ab83fc8f8804a1b46c
LiuLiu <13520782708@139.com>
2020-12-09 12:20:21 +0800

    rank

---
+++ rank/src/com/company/TreeNode.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.*;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public static List<TreeNode> createBinTree(int[] array) {
+        List<TreeNode> nodeList = new LinkedList<TreeNode>();
+        // 将一个数组的值依次转换为Node节点
+        for (int nodeIndex = 0; nodeIndex < array.length; nodeIndex++) {
+            nodeList.add(new TreeNode(array[nodeIndex]));
+        }
+        // 对前lastParentIndex-1个父节点按照父节点与孩子节点的数字关系建立二叉树
+        for (int parentIndex = 0; parentIndex < array.length / 2 - 1; parentIndex++) {
+            // 左孩子
+            nodeList.get(parentIndex).left = nodeList.get(parentIndex * 2 + 1);
+            // 右孩子
+            nodeList.get(parentIndex).right = nodeList.get(parentIndex * 2 + 2);
+        }
+        // 最后一个父节点:因为最后一个父节点可能没有右孩子，所以单独拿出来处理
+        int lastParentIndex = array.length / 2 - 1;
+        // 左孩子
+        nodeList.get(lastParentIndex).left = nodeList.get(lastParentIndex * 2 + 1);
+        // 右孩子,如果数组的长度为奇数才建立右孩子
+        if (array.length % 2 == 1) {
+            nodeList.get(lastParentIndex).right = nodeList.get(lastParentIndex * 2 + 2);
+        }
+        return nodeList;
+    }
+
+}
